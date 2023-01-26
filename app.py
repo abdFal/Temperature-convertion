682482964066773cc881d73fa3cead532e2d79b1
unknown <naufaldec07@gmail.com>
2023-01-26 09:46:14 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def convert():
+    if request.method == 'POST':
+        celsius = float(request.form['celsius'])
+        fahrenheit = (celsius * 9/5) + 32
+        return str(fahrenheit) + ' degrees Fahrenheit'
+    return '''
+        <form method="post">
+            Celsius: <input type="text" name="celsius"><br>
+            <input type="submit" value="Convert to Fahrenheit">
+        </form>
+    '''
+if __name__ == '__main__':
+    app.run(debug=True)
